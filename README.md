5366ca6e6f7397a0e5fa9be2398c63ce55252b0a
z5bra <z5bra@protonmail.com>
2024-04-01 01:08:56 +0800

    上传

---
+++ README.md
@@ -0,0 +1,3 @@
+OWASP TOP 10(2021) + CWE
+[CWE-22 路径遍历](https://cwe.mitre.org/data/definitions/22.html)
+
---
+++ internal/web/index.go
@@ -0,0 +1,10 @@
+package web
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// Index主页
+func Index(c *gin.Context) {
+
+}
