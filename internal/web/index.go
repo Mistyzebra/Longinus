934e83d3cea14cf70863b328d14578fbf3d2d90a
z5bra <z5bra@protonmail.com>
2024-04-01 14:38:31 +0800

    更新

--- internal/web/index.go
+++ internal/web/index.go
@@ -1,10 +1,14 @@
 package web
 
 import (
+	"net/http"
 	"github.com/gin-gonic/gin"
 )
 
-// Index主页
+// Index home page
 func Index(c *gin.Context) {
-
+	c.HTML(http.StatusOK, "index", gin.H{
+		"title": "首页",
+		"ctx":   c,
+	})
 }
